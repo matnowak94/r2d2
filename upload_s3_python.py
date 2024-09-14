72a927fe8319ad96da3d63834e6d7d2c67e5d611
mateusz.nowak <mateusz.nowak@emlpayments.com>
2024-09-14 15:54:33 +0200

    Add function, reformat writing to file, use subprocess

--- upload_s3_python.py
+++ upload_s3_python.py
@@ -9,72 +9,80 @@ import os
 
 # Collecting information about EC2 instance from AWS service
 
-user_data = 'http://169.254.169.254/latest/user-data'
-meta_data = 'http://169.254.169.254/latest/meta-data'
-ec2InsDatafile = 'ec2InsDatafile'
+user_data = "http://169.254.169.254/latest/user-data"
+meta_data = "http://169.254.169.254/latest/meta-data"
+ec2InsDatafile = "ec2InsDatafile"
 ec2_params = {
-    'instance_id': 'instance-id',
-    'reservation_id': 'reservation-id',
-    'public_ip': 'public-ipv4',
-    'public_hostname': 'public-hostname',
-    'private_ip':'local-ipv4',
-    'security_groups':'security-groups',
-    'ami_id': 'ami-id'
+    "Instance ID": "instance-id",
+    "Reservation ID": "reservation-id",
+    "Public IP": "public-ipv4",
+    "Public Hostname": "public-hostname",
+    "Private IP": "local-ipv4",
+    "Security Groups": "security-groups",
+    "AMI ID": "ami-id",
 }
 
+# Subprocess was not used, but I assume it's a hint to use it instead of popen
+# Also, as we're runnning 3 different commands we can create a function to handle them
+def check_os_options(command):
+    try:
+        result = subprocess.check_output(command, shell=True, text=True).strip()
+        return result
+    except Exception as e:
+        print(f"Error while running command {command} : {e}")
+        return None
+
+
 try:
-    with open(ec2InsDatafile, 'w') as fh:
+    with open(ec2InsDatafile, "w") as fh:
         for param, value in ec2_params.items():
             try:
-                response = requests.get(meta_data +'/' + value)
+                response = requests.get(meta_data + "/" + value)
+                if isinstance(response.text, list):
+                    print(response.text + ": is a list")
+                    data = " ".join(response.text)
+                else:
+                    data = param + ":" + response.text
+                fh.write(data + "\r\n")
             except Exception as e:
                 print(f"Error while making request: {e}")
 
-        if isinstance(response.text, list):
-            print(response.text + ': is a list')
-            data = ' '.join(response.text)
-        else:
-            data = param + ":" + response.text
-    
+        os_vers = "grep '^VERSION=' /etc/os-release | cut -d'=' -f2"
+        os_name = "grep '^NAME' /etc/os-release | cut -d'=' -f2"
+        os_usrs = "grep -E 'bash|sh' /etc/passwd | awk -F : '{print $1}' | xargs echo"
+
+        os_name_val = "OS_NAME:" + check_os_options(os_name)
+        os_vers_val = "OS_VERSION:" + check_os_options(os_vers)
+        os_usrs_val = "Login able users:" + check_os_options(os_usrs)
+
         try:
-            fh.write(data+'\r\n')
-        except Exception as e:
-            print(f"Error during writing to file: {e}")
-            print(data)
+            fh.write(os_name_val + "\r\n")
+            fh.write(os_vers_val + "\r\n")
+            fh.write(os_usrs_val + "\r\n")
+        except:
+            print(f"Error during write to file: {e}")
+            fh.close()
 
-except:
-    print('Error while opening file for write')
- 
-#Getting  OS related if from system files
-
-os_vers = "grep '^VERSION=' /etc/os-release |cut -d'=' -f2"
-os_name = "grep '^NAME' /etc/os-release |cut -d'=' -f2"
-os_name_val ='OS NAME: '+ os.popen(os_name).read().rstrip()
-os_vers_val ='OS VERSION: '+ os.popen(os_vers).read().rstrip()
-os_usrs = "grep -E 'bash|sh' /etc/passwd |awk -F : '{print $1}|xargs echo  "
-os_usrs_val = 'Login able users: '+ os.popen(os_usrs).read().rstrip()
-try:
-    fh.write(os_name_val+'\r\n')
-    fh.write(os_vers_val+'\r\n')
-    fh.write(os_usrs_val+'\r\n')
-except:
-    print "Error during write to file"
-    fh.close()
-
+except OSError as e:
+    print(f"Error while opening file for write: {e}")
 
 # Upload file to s3 storage
-s3_bucket_name = 'new-bucket-e05ab0e0'
-s3_conn = boto3.client('s3')
+s3_bucket_name = "new-bucket-e05ab0e0"
+s3_conn = boto3.client("s3")
 
 try:
     s3.meta.client.head_bucket(Bucket=s3_bucket_name)
 
-    with (ec2InsDatafile, 'r') as fh:
+    with (ec2InsDatafile, "r") as fh:
         s3_conn.put_object(
             Bucket=s3_bucket_name,
-            Key='system_info' + requests.get(meta_data +'/' + instance_id) + '.txt',
-            Body=fh.read()
+            Key="system_info" + requests.get(meta_data + "/" + instance_id) + ".txt",
+            Body=fh.read(),
         )
-    print("File has been uploaded into " + s3_bucket_name + " S3 bucket with instance_id key.")
+    print(
+        "File has been uploaded into "
+        + s3_bucket_name
+        + " S3 bucket with instance_id key."
+    )
 except ClientError:
-    "Are you sure the destination bucket exist? Check it."
+    print("Are you sure the destination bucket exist? Check it.")
